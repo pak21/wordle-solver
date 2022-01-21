9273a0e96a012ea8be0c372a39b648be8f850bb2
Philip Kendall <philip@shadowmagic.org.uk>
2022-01-21 11:20:02 +0000

    Minor simplifications/performance improvements.

--- solve.py
+++ solve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import collections
 import itertools
 from operator import itemgetter
 
@@ -18,31 +19,28 @@ with open(args.words) as f:
     words = {l.strip() for l in f}.union(all_answers)
 
 def worst_case(answers, guess):
-    sigs = sorted([(wordle.signature(a, guess), a) for a in answers])
-    grouped = itertools.groupby(sigs, key=itemgetter(0))
-    return max([len(list(v)) for _, v in grouped])
+    sigs = [wordle.signature(a, guess) for a in answers]
+    return max(collections.Counter(sigs).values())
 
 def get_next_guess(possibles, words):
-    potential_guesses = sorted([(worst_case(possibles, w), not(w in possibles), w) for w in words])
-    worst_case_count, _, next_guess = potential_guesses[0]
+    potential_guesses = [(worst_case(possibles, w), not(w in possibles), w) for w in words]
+    worst_case_count, _, next_guess = min(potential_guesses)
     return next_guess, worst_case_count
 
 def recurse(answers, guess, depth):
     indent = '  ' * depth
-    all_signatures = {wordle.signature(a, guess) for a in answers}
+    all_signatures = sorted([(wordle.signature(a, guess), a) for a in answers])
+    grouped = itertools.groupby(all_signatures, key=itemgetter(0))
 
-    for sig in sorted(all_signatures):
+    for sig, p in grouped:
+        possibles = [w for _, w in p]
         if sig == 'ggggg':
             print(f'{indent}{sig}: {guess} is correct after {depth} guesses')
         else:
-            possibles = [a for a in answers if wordle.signature(a, guess) == sig]
             next_guess, worst_case_count = get_next_guess(possibles, words)
             print(f'{indent}{sig}: next guess should be {next_guess} which leaves a worst case of {worst_case_count}')
             recurse(possibles, next_guess, depth + 1)
 
-if True:
-    first_guess, worst_case_count = get_next_guess(all_answers, words)
-    print(f'First guess should be {first_guess} which leaves a worst case of {worst_case_count}')
-    recurse(all_answers, first_guess, 1)
-else:
-    recurse(all_answers, 'raise', 1)
+first_guess, worst_case_count = get_next_guess(all_answers, words)
+print(f'First guess should be {first_guess} which leaves a worst case of {worst_case_count}')
+recurse(all_answers, first_guess, 1)
