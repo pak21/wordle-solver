658059a1490d6e32a0ba628a743c0874eecf229e
Philip Kendall <philip@shadowmagic.org.uk>
2022-01-22 21:44:24 +0000

    Add ability to force first guess.

--- solve.py
+++ solve.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--answers', required=True, help='Answer list')
 parser.add_argument('-w', '--words', required=True, help='Word list')
 parser.add_argument('--hard', action='store_true', help='Hard mode')
+parser.add_argument('-f', '--first', help='Forced first guess')
 args = parser.parse_args()
 
 with open(args.answers) as f:
@@ -27,7 +28,7 @@ def get_next_guess(possibles, words):
     potential_guesses = sorted([(worst_case(possibles, w), w) for w in words])
     worst_case_count = potential_guesses[0][0]
     tied_guesses = [(wordle.score(guess, possibles), guess) for count, guess in potential_guesses if count == worst_case_count]
-    return min(tied_guesses)[1], worst_case_count
+    return max(tied_guesses)[1], worst_case_count
 
 def recurse(answers, guess, words, hard_mode, depth):
     indent = '  ' * depth
@@ -50,6 +51,10 @@ def recurse(answers, guess, words, hard_mode, depth):
             print(f'{indent}{sig}: next guess should be {next_guess} which leaves a worst case of {worst_case_count}/{len(possibles)}')
             recurse(possibles, next_guess, filtered_words, hard_mode, depth + 1)
 
-first_guess, worst_case_count = get_next_guess(all_answers, all_words)
+if args.first:
+    first_guess, worst_case_count = args.first, '???'
+else:
+    first_guess, worst_case_count = get_next_guess(all_answers, all_words)
+
 print(f'First guess should be {first_guess} which leaves a worst case of {worst_case_count}/{len(all_answers)}')
 recurse(all_answers, first_guess, all_words, args.hard, 1)
